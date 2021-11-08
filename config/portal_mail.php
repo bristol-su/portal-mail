b64922c086250340f50e398bca65d9fb4f17cf5e
Toby Twigger <tobytwigger1@gmail.com>
2021-11-08 18:33:46 +0000

    Update config to add PORTAL_MAIL_AWS_REGION

--- config/portal_mail.php
+++ config/portal_mail.php
@@ -6,7 +6,7 @@ return [
             'key'    => env('AWS_ACCESS_KEY_ID', ''),
             'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
         ],
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'region' => env('PORTAL_MAIL_AWS_REGION', env('AWS_DEFAULT_REGION', 'us-east-1')),
         'version' => 'latest',
         'ua_append' => [
             'L5MOD/' . \BristolSU\Mail\MailServiceProvider::VERSION,
