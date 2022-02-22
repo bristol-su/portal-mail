36e96b7590c686aa7c9bf70754ab574cad23309a
Toby Twigger <tobytwigger1@gmail.com>
2022-02-22 16:48:50 +0000

    Remove log for missing uuid

--- src/Capture/Listeners/MailSentListener.php
+++ src/Capture/Listeners/MailSentListener.php
@@ -12,24 +12,20 @@ class MailSentListener
 
     public function handle(MessageSent $event)
     {
-        if (isset($event->data['__bristol_su_mail_uuid'])) {
-            $sentMail = SentMail::where('uuid', $event->data['__bristol_su_mail_uuid'])->first();
-            if($sentMail) {
-                $sentMail->fill([
-                    'is_sent' => true,
-                    'is_error' => false,
-                    'error_message' => null,
-                    'sent_at' => Carbon::now()
-                ])->save();
-                if(($parent = $sentMail->parentEmail) !== null && $parent->sent_at === null) {
-                    $parent->sent_id = Carbon::now();
-                    $parent->save();
-                }
-            } else {
-                Log::info('Mail sent without recording the information. ' . json_encode($event->data));
+        $sentMail = SentMail::where('uuid', $event->data['__bristol_su_mail_uuid'])->first();
+        if($sentMail) {
+            $sentMail->fill([
+                'is_sent' => true,
+                'is_error' => false,
+                'error_message' => null,
+                'sent_at' => Carbon::now()
+            ])->save();
+            if(($parent = $sentMail->parentEmail) !== null && $parent->sent_at === null) {
+                $parent->sent_id = Carbon::now();
+                $parent->save();
             }
         } else {
-            Log::info('Mail sent without recording the information dur to missing uuid. ' . json_encode($event->data));
+            Log::info('Mail sent without recording the information. ' . json_encode($event->data));
         }
     }
 
