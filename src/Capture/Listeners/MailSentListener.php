651a5e8e559f787d1b36c4e9d6ca019c54c441df
Toby Twigger <tobytwigger1@gmail.com>
2022-02-23 14:11:51 +0000

    Update MailSentListener.php

--- src/Capture/Listeners/MailSentListener.php
+++ src/Capture/Listeners/MailSentListener.php
@@ -12,20 +12,24 @@ class MailSentListener
 
     public function handle(MessageSent $event)
     {
-        $sentMail = SentMail::where('uuid', $event->data['__bristol_su_mail_uuid'])->first();
-        if($sentMail) {
-            $sentMail->fill([
-                'is_sent' => true,
-                'is_error' => false,
-                'error_message' => null,
-                'sent_at' => Carbon::now()
-            ])->save();
-            if(($parent = $sentMail->parentEmail) !== null && $parent->sent_at === null) {
-                $parent->sent_id = Carbon::now();
-                $parent->save();
+        $uuid = isset($event->data['__bristol_su_mail_uuid']) ? $event->data['__bristol_su_mail_uuid'] : null;
+        if($uuid) {
+            $sentMail = SentMail::where('uuid', $uuid)->first();
+            if($sentMail) {
+                $sentMail->fill([
+                    'is_sent' => true,
+                    'is_error' => false,
+                    'error_message' => null,
+                    'sent_at' => Carbon::now()
+                ])->save();
+                if(($parent = $sentMail->parentEmail) !== null && $parent->sent_at === null) {
+                    $parent->sent_id = Carbon::now();
+                    $parent->save();
+                }
+            } else {
+                Log::info('Mail sent without recording the information. ' . json_encode($event->data));
             }
-        } else {
-            Log::info('Mail sent without recording the information. ' . json_encode($event->data));
+
         }
     }
 
