5567d5f0ea0df3edaa59725fe02b8bb4c254bfe7
Toby Twigger <tobytwigger1@gmail.com>
2022-04-05 16:31:30 +0100

    Change migration to anonymous

--- database/migrations/2022_04_05_235200_update_portal_mail_attachments_table_set_sent_mail_id_to_nullable.php
+++ database/migrations/2022_04_05_235200_update_portal_mail_attachments_table_set_sent_mail_id_to_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePortalMailAttachmentsTableSetSentMailIdToNullable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -25,5 +25,6 @@ class CreatePortalMailAttachmentsTableSetSentMailIdToNullable extends Migration
      */
     public function down()
     {
+
     }
-}
+};
